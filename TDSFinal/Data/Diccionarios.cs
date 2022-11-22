5df3c3e572faf909dc4a15e07b4ad0453818644b
Gerson Gil <gersongilfer@gmail.com>
2022-11-22 08:39:04 -0400

    fixing errores

---
+++ TDSFinal/Data/Diccionarios.cs
@@ -0,0 +1,10 @@
+﻿namespace TDSFinal.Data
+{
+    public class Diccionarios
+    {
+        public Dictionary<string, string> ProvinciasTuristicas = new Dictionary<string, string>();
+
+        //public Dictionary<string, string>  = new Dictionary<string, string>();
+
+    }
+}
